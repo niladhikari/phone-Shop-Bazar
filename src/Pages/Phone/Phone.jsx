8942a4e791f1ae95ca2efb9bafe0ae803ed2f07a
niladhikari <niladhikari445@gmail.com>
2023-09-23 18:14:43 +0600

    add aall

--- src/Pages/Phone/Phone.jsx
+++ src/Pages/Phone/Phone.jsx
@@ -1,12 +1,21 @@
-import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
 
 
 const Phone = () => {
-    const phone = useLoaderData()
+    const[phone,setPhone] = useState({})
+    const {id} = useParams()
+
+    const phones = useLoaderData()
+
+    useEffect(()=>{
+        const findPhone = phones?.find((phone)=> phone.id === id)
+        setPhone(findPhone);
+    },[id,phones])
     
     return (
         <div>
-            
+            <h2>Phone:{phone.id}</h2>
         </div>
     );
 };
